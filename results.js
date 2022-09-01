0a5df932dc5eb82b201f1fbc3b4a1cbb174058af
Banasing-Sahadatu <banasingsahadatu@gmail.com>
2022-09-01 14:33:44 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,11 @@
+const results = [{ name: "Sahadatu Banasing",class:"form four", grade: "7" }, 
+                 { name: "Badran Pious", class: "form four", grade: "14" },
+                 { name: "Shamsideen Yigri", class: "form four", grade: "8"},
+                 { name: "Mariam Adams", class: "form four", grade: "16" },
+                 { name: "Hikmat Fuseini",class:"form four", grade: "6" },
+    
+    
+    
+]
+
+module.exports = results;
